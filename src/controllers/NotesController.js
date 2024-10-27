a006962b88add80601b1a42d252e817a07fac1fa
Yaba Ernesto <yabaernesto@gmail.com>
2024-10-27 19:15:54 +0100

    feat: listagem de notas adicionado no NoteController

--- src/controllers/NotesController.js
+++ src/controllers/NotesController.js
@@ -57,6 +57,53 @@ class NotesController {
       links
     })
   }
+
+  async index(request, response) {
+    const { user_id } = request.query
+
+    let notes
+
+    if (tags) {
+      const filterTags = tags.split(',').map(tag => tag.trim())
+
+      notes = await knex("tags")
+        .select([
+          "notes.id",
+          "notes.title",
+          "notes.user_id"
+        ])
+        .where("notes.user_id", user_id)
+        .whereLike("notes.title", `%${title}%`)
+        .whereIn("name", filterTags)
+        .innerJoin("notes", "notes.id", "tags.note_id")
+        .orderBy("notes.title")
+    } else {
+      notes = await knex("notes")
+        .where({ user_id })
+        .whereLike("title", `%${title}%`)
+        .orderBy("title")
+    }
+
+    const userTags = await knex("tags").where({ user_id })
+    const notesWithTags = notes.map(note => {
+      const noteTags = userTags.filter(tag => tag.note_id === note.id)
+
+      return {
+        ...note,
+        tags: noteTags
+      }
+    })
+
+    return response.json(notesWithTags)
+  }
+
+  async delete(request, response) {
+    const { id } = request.body
+
+    await knex("notes").where({ id }).delete()
+
+    return response.json()
+  }
 }
 
 module.exports = NotesController
