3ec429cbe535721698fa84c92f02869e6cfbb7fc
Yaba Ernesto <yabaernesto@gmail.com>
2024-10-27 19:54:21 +0100

    feat: controlador de tags adicionada

--- src/routes/index.js
+++ src/routes/index.js
@@ -2,9 +2,11 @@ const { Router } = require('express')
 
 const usersRouter = require('./users.routes.js')
 const notesRouter = require('./notes.routes.js')
+const tagsRouter = require('./tags.routes.js')
 
 const router = Router()
 router.use('/users', usersRouter)
 router.use('/notes', notesRouter)
+router.use('/tags', tagsRouter)
 
 module.exports = router
