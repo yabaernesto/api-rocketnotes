1b45db153f772aa00c0aa40fbce9dce62f71a269
Yaba Ernesto <yabaernesto@gmail.com>
2024-10-27 21:53:38 +0100

    feat: jwt e token de autenticacao adicionados

--- src/routes/index.js
+++ src/routes/index.js
@@ -3,9 +3,11 @@ const { Router } = require('express')
 const usersRouter = require('./users.routes.js')
 const notesRouter = require('./notes.routes.js')
 const tagsRouter = require('./tags.routes.js')
+const sessionsRouter = require('./sessions.routes.js')
 
 const router = Router()
 router.use('/users', usersRouter)
+router.use('/sessions', sessionsRouter)
 router.use('/notes', notesRouter)
 router.use('/tags', tagsRouter)
 
